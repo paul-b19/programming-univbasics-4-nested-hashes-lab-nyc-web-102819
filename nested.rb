cbc2bac31eb5ddf8ce98bf7ef3f24c1137aad663
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-16 22:28:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,6 @@ def hopper
         }
      }
   programmer_hash[:grace_hopper]
-
 end
 
 def alan_kay_is_known_for
@@ -83,7 +82,6 @@ def adding_matz
      }
   programmer_hash[:yukihiro_matsumoto] = {}
   programmer_hash
-    
 end
 
 def changing_alan
@@ -104,8 +102,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
